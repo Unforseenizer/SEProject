e43a82977ae5498950610624a52fec6e61e7fff4
TanakaSakana <tanakatosakana@gmail.com>
2016-04-19 22:31:57 +0800

    debug all

--- app/src/main/java/com/example/desktop/event/NetUtil.java
+++ app/src/main/java/com/example/desktop/event/NetUtil.java
@@ -102,7 +102,6 @@ public class NetUtil {
             super.onPostExecute(res);
             pd.dismiss();
             Log.e("result", "onPostExecute");
-            ((Activity) mContext).finish();
             Toast.makeText(mContext, "Server REPLY : " + res, Toast.LENGTH_SHORT).show();
         }
     }
